9c17ecafe85e7d29ee8b2073edc28822ab4dfb59
sven_beyel <sven.junker-beyel@gv.hamburg.de>
2023-04-21 16:05:47 +0200

    add addons streetSmart to addons_3_0_0
    BG-3813

---
+++ addons_3_0_0/streetSmart/store/gettersStreetSmart.js
@@ -0,0 +1,32 @@
+import {generateSimpleGetters} from "../../../../src_3_0_0/shared/js/utils/generators";
+import stateStreetSmart from "./stateStreetSmart";
+
+const getters = {
+    ...generateSimpleGetters(stateStreetSmart),
+
+    /**
+     * Returns the dedicated supported locale of StreetsmartApi, if available, else returns "de".
+     * The following locales are supported by StreetsmartApi v22: de, en-GB, en-US, fi, fr, nl, pt-BR.
+     * @param {object} ___ streetsmart store state
+     * @param {object} __ streetsmart store getters
+     * @param {object} _ root state
+     * @param {object} rootGetters root getters
+     * @returns {string} the current locale if supported or "de"
+     */
+    currentLocale (___, __, _, rootGetters) {
+        let locale = rootGetters["Language/currentLocale"];
+
+        if (locale === "en") {
+            locale = "en-US";
+        }
+        else if (locale === "pt") {
+            locale = "pt-BR";
+        }
+        else {
+            locale = "de";
+        }
+        return locale;
+    }
+};
+
+export default getters;
---
+++ addons_3_0_0/streetSmart/store/mutationsStreetSmart.js
@@ -0,0 +1,9 @@
+import {generateSimpleMutations} from "../../../../src_3_0_0/shared/js/utils/generators";
+import stateStreetSmart from "./stateStreetSmart";
+
+const mutations = {
+    ...generateSimpleMutations(stateStreetSmart)
+
+};
+
+export default mutations;
