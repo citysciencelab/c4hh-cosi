81cd667f63f40c732d129cad2613eb207e92b82a
Klara_B <klara.boecker@gv.hamburg.de>
2023-06-13 11:43:42 +0200

    BG-3814 removed flaecheninfo and fixed schulinfo

--- addons_3_0_0/gfiThemes/schulinfo/components/SchulinfoTheme.vue
+++ addons_3_0_0/gfiThemes/schulinfo/components/SchulinfoTheme.vue
@@ -3,13 +3,13 @@ import ThemeConfig from "../themeConfig.json";
 import {isWebLink} from "../../../../../src_3_0_0/shared/js/utils/urlHelper.js";
 import {isPhoneNumber, getPhoneNumberAsWebLink} from "../../../../../src_3_0_0/shared/js/utils/isPhoneNumber.js";
 import {isEmailAddress} from "../../../../../src_3_0_0/shared/js/utils/isEmailAddress.js";
-import CompareFeatureIcon from "../../../../../src/modules/tools/gfi/components/favoriteIcons/components/CompareFeatureIcon.vue";
+// import CompareFeatureIcon from "../../../../../src_3_0_0/modules/getFeatureInfo/components/favoriteIcons/components/CompareFeatureIcon.vue";
 // import TargetSchoolIcon from "./favoriteIcons/TargetSchoolIcon.vue";
 
 export default {
     name: "SchulinfoTheme",
     components: {
-        CompareFeatureIcon
+        // CompareFeatureIcon
         // TargetSchoolIcon
     },
     props: {
