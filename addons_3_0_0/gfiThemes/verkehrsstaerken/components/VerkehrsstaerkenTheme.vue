094494e2a6f62695ba46923287c7f0717c1ba7e5
fmlgv <friedrich.mueller@gv.hamburg.de>
2023-07-14 16:21:39 +0200

    add PR #684

--- addons_3_0_0/gfiThemes/verkehrsstaerken/components/VerkehrsstaerkenTheme.vue
+++ addons_3_0_0/gfiThemes/verkehrsstaerken/components/VerkehrsstaerkenTheme.vue
@@ -22,14 +22,19 @@ export default {
     data () {
         return {
             activeTab: "table",
-            downloadLink: "https://daten-hamburg.de/transport_verkehr/verkehrsstaerken/DTV_DTVw_Download.xlsx",
             years: [],
             rowNames: [],
             dataset: []
         };
     },
     computed: {
-        ...mapGetters("Modules/Language", ["currentLocale"])
+        ...mapGetters("Modules/Language", ["currentLocale"]),
+        gfiParams: function () {
+            return this.feature.getTheme()?.params;
+        },
+        downloadLink: function () {
+            return this.gfiParams?.downloadLink ? this.gfiParams.downloadLink : "https://daten-hamburg.de/transport_verkehr/verkehrsstaerken/DTV_DTVw_Download.xlsx";
+        }
     },
     watch: {
         // When the gfi window switched with arrow, the connection will be refreshed
