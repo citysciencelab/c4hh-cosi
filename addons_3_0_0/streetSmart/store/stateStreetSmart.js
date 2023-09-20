ef7236c1c1516dbc116f8309963cebd2d918b1f2
Klara_B <klara.boecker@gv.hamburg.de>
2023-09-20 13:06:14 +0200

    added descriptions in modules

--- addons_3_0_0/streetSmart/store/stateStreetSmart.js
+++ addons_3_0_0/streetSmart/store/stateStreetSmart.js
@@ -1,7 +1,7 @@
 /**
  * User type definition
  * @typedef {Object} StreetSmartState
- * @property {String} description The description that should be shown in the button in the menu.
+ * @property {String} description description of tool (config-param)
  * @property {Boolean} hasMouseMapInteractions If this attribute is true, then all other modules will be deactivated when this attribute is also true. Only one module with this attribute true may be open at the same time, since conflicts can arise in the card interactions.
  * @property {String} icon icon next to title (config-param)
  * @property {String} name displayed as title (config-param)
