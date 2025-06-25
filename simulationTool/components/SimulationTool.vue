af2acb231f9cee0b5486b545e983a5dfc9d4dca2
DargeFi <finn-rasmus.darge@dataport.de>
2025-06-25 08:40:25 +0200

    add print trigger button

--- simulationTool/components/SimulationTool.vue
+++ simulationTool/components/SimulationTool.vue
@@ -127,6 +127,9 @@ export default {
             <simulationResults
                 v-if="mode === 'simulationResults'"
             />
+            <SimulationMockData
+                v-if="mode === 'mockData'"
+            />
             <ProcessList
                 v-if="mode === 'process-list'"
                 @selected="selectProcess"
