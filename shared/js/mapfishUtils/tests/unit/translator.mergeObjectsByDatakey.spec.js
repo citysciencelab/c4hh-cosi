ef50314e1aafd6ca35b92a3de8603136c8ee25da
Weronika Natur <weronika.natur@gv.hamburg.de>
2025-07-31 10:56:59 +0200

    fix

--- shared/js/mapfishUtils/tests/unit/translator.mergeObjectsByDatakey.spec.js
+++ shared/js/mapfishUtils/tests/unit/translator.mergeObjectsByDatakey.spec.js
@@ -177,7 +177,7 @@ describe("addons/shared/js/mapfishUtils/translator.mergeObjectsByDatakey.js", ()
                         "type": "Number",
                         "postfix": " m²",
                         "decimals": 2,
-                        "thousandsSeparator.js": {
+                        "thousandsSeparator": {
                             "delimAbs": ".",
                             "delimDec": ","
                         }
