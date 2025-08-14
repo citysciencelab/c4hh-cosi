2d9252e2a18eed83ffd744f10a86dfd5a5bfc5d7
Inka Dudek <inka.dudek@gv.hamburg.de>
2025-08-14 17:40:00 +0200

    BG-6689 - removed circular dependencies in imports

--- shared/js/mapfishUtils/createKnowledgeBase.js
+++ shared/js/mapfishUtils/createKnowledgeBase.js
@@ -3,7 +3,6 @@ import rawLayerList from "@masterportal/masterportalapi/src/rawLayerList";
 import nextFeatureByDistance from "./precompiler.nextFeatureByDistance.js";
 import allFeaturesByDuration from "./precompiler.allFeaturesByDuration.js";
 import {nextGroupedFeaturesByDistance} from "./precompiler.nextGroupedFeaturesByDistance.js";
-import sumNumbers from "./precompiler.sumNumbers.js";
 
 /**
  * Creates the knowledge base by the services config.
@@ -137,6 +136,69 @@ export function createKnowledgeBase (parcelData, services, {mapProjection, oafCR
         addKnowledgeBaseError(knowledgeBase, error, prefix, config?.propertyName);
         createKnowledgeBase(parcelData, services, {mapProjection, oafCRSURI}, onstart, onfinish, onUserError, onDevError, knowledgeBase, idx + 1);
     });
+}
+
+/**
+ * A function to return a sum of numbers from the propertyName
+ * @param {ol/Feature[]} features An array of features to generate the attributes
+ * @param {String} knowledgeBaseKey The key to use for sum in the result.
+ * @param {String} knowledgeBaseSum The property name from which to get the value.
+ * @param {String[]} propertyName All attribute that should be additionally put into the result.
+ * @param {Function} onsuccess A function function(data) with data as the resulting entry for the knowledge base.
+ * @param {Function} onerror A function to call on error as function(error) with error an instance of Error.
+ * @returns {void}
+ */
+function sumNumbers (features, knowledgeBaseKey, knowledgeBaseSum, propertyName, onsuccess, onerror) {
+    if (!Array.isArray(features)) {
+        if (typeof onerror === "function") {
+            onerror(new Error("sumNumbers: features is not an array"));
+        }
+        return;
+    }
+    else if (typeof knowledgeBaseKey !== "string") {
+        if (typeof onerror === "function") {
+            onerror(new Error("sumNumbers: the key to be summed is needed"));
+        }
+        return;
+    }
+    else if (typeof knowledgeBaseSum !== "string") {
+        if (typeof onerror === "function") {
+            onerror(new Error("sumNumbers: the propertyName to be summed is needed"));
+        }
+        return;
+    }
+    else if (!Array.isArray(propertyName)) {
+        if (typeof onerror === "function") {
+            onerror(new Error("sumNumbers: the propertyName should be in an array"));
+        }
+        return;
+    }
+    else if (!features.length) {
+        if (typeof onsuccess === "function") {
+            onsuccess({});
+        }
+        return;
+    }
+
+    const result = createAttributesByFeatures(features, propertyName);
+
+    if (propertyName.includes(knowledgeBaseSum)) {
+        result[knowledgeBaseKey] = 0;
+
+        if (typeof result[knowledgeBaseSum] === "string") {
+            result[knowledgeBaseKey] = result[knowledgeBaseSum];
+        }
+        else if (Array.isArray(result[knowledgeBaseSum])) {
+            result[knowledgeBaseSum].forEach(value => {
+                result[knowledgeBaseKey] += parseFloat(value);
+            });
+            result[knowledgeBaseKey] = [String(result[knowledgeBaseKey])];
+        }
+    }
+
+    if (typeof onsuccess === "function") {
+        onsuccess(result);
+    }
 }
 
 /**
