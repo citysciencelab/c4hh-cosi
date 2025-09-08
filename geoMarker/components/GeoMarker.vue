63669f5e466540ac0b4c702816066efca4b4446f
zehbeck <christa.becker@gv.hamburg.de>
2025-09-08 12:03:00 +0200

    G31DEV2-152 add navigation tabs

--- geoMarker/components/GeoMarker.vue
+++ geoMarker/components/GeoMarker.vue
@@ -1,17 +1,90 @@
 <script>
+import NavTab from "@shared/modules/tabs/components/NavTab.vue";
+
 export default {
-    name: "GeoMarker"
+    name: "GeoMarker",
+    components: {
+        NavTab
+    }
 };
 </script>
 
 <template>
     <div id="geoMarker">
-        TODO
+        <ul
+            id="geoMarkerTabs"
+            class="nav nav-tabs nav-justified"
+            role="tablist"
+        >
+            <NavTab
+                id="newTab"
+                :active="false"
+                target="#newTabPane"
+                label="Neu"
+            />
+
+            <NavTab
+                id="filterTab"
+                :active="true"
+                target="#filterTabPane"
+                label="Filter"
+            />
+
+            <NavTab
+                id="listTab"
+                :active="false"
+                target="#listTabPane"
+                label="Liste"
+            />
+        </ul>
+
+        <div
+            id="geomarkerTabContent"
+            class="tab-content"
+        >
+            <div
+                id="newTabPane"
+                class="tab-pane fade"
+                role="tabpanel"
+                aria-labelledby="new-tab"
+                tabindex="0"
+            >
+                <p>Neuen GeoMarker anlegen</p>
+            </div>
+
+            <div
+                id="filterTabPane"
+                class="tab-pane fade show active"
+                role="tabpanel"
+                aria-labelledby="filter-tab"
+                tabindex="0"
+            >
+                <p>Filter GeoMarker</p>
+            </div>
+
+            <div
+                id="listTabPane"
+                class="tab-pane fade"
+                role="tabpanel"
+                aria-labelledby="list-tab"
+                tabindex="0"
+            >
+                <p>gefilterte GeoMarker auflisten</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
-#geoMarker {
-    background-color: #EEE;
+div#geoMarker {
+    ul#geoMarkerTabs {
+        button.nav-link {
+            padding: 0.5rem;
+        }
+    }
+
+    div#geomarkerTabContent {
+        padding-top: 1rem;
+    }
 }
 </style>
--- geoMarker/tests/unit/components/GeoMarker.spec.js
+++ geoMarker/tests/unit/components/GeoMarker.spec.js
@@ -5,10 +5,28 @@ import GeoMarker from "../../../components/GeoMarker.vue";
 config.global.mocks.$t = key => key;
 
 describe("addons/geoMarker/components/GeoMarker.vue", () => {
-    it("should exist and have the correct id", async () => {
-        const wrapper = shallowMount(GeoMarker);
+    let wrapper;
 
+    beforeEach(() => {
+        wrapper = wrapper = shallowMount(GeoMarker);
+    });
+
+    afterEach(() => {
+        if (wrapper) {
+            wrapper.unmount();
+        }
+    });
+
+
+    it("should exist and have the correct id", () => {
         expect(wrapper.exists()).to.be.true;
         expect(wrapper.attributes("id")).to.be.equal("geoMarker");
+    });
+
+    it("should have all navigation tabs", () => {
+        expect(wrapper.find("#newTab").exists()).to.be.true;
+        expect(wrapper.find("#listTab").exists()).to.be.true;
+        expect(wrapper.find("#filterTab").exists()).to.be.true;
+        expect(wrapper.find("#filterTab").attributes("active")).to.equal("true");
     });
 });
