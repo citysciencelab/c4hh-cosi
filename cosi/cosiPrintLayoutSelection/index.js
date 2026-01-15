0570ce08ed9fd20cae0b16a0ebf8b0d264a9e72c
Jakob Ahrens <jakob.ahrens@gv.hamburg.de>
2026-01-15 10:03:56 +0100

    G31DEV1-3470-print-ui in custom addon

---
+++ cosi/cosiPrintLayoutSelection/index.js
@@ -0,0 +1,8 @@
+import component from "./components/CosiPrintLayoutSelection.vue";
+import de from "./locales/de/additional.json";
+import en from "./locales/en/additional.json";
+
+export default {
+    component,
+    locales: {de, en}
+};
