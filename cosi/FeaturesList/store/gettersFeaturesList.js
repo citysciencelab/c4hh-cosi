ecf4d270e5147515ba1183d605112541487fbce1
Feng Feng <feng.feng@gv.hamburg.de>
2026-02-03 08:16:29 +0100

    G31DEV1-3663 Fix: the feature list only shows the features on the map.
    Because of the fitlered results, not all the features of layer will be
    shown on map.

--- cosi/FeaturesList/store/gettersFeaturesList.js
+++ cosi/FeaturesList/store/gettersFeaturesList.js
@@ -95,5 +95,7 @@ export default {
     },
     layerMapById: (state, {flatActiveLayerMapping}) => id => flatActiveLayerMapping.find(l => l.layerId === id),
     isFeatureDisabled: (state, {disabledFeatureItems}) => feature => disabledFeatureItems.filter(item => item.feature === feature).length > 0,
-    isFeatureActive: (state, {isFeatureDisabled}) => feature => (typeof feature.style_ === "object" || feature.style_ === null || feature.style_ === undefined) && !isFeatureDisabled(feature)
+    isFeatureActive: (state, {isFeatureDisabled}) => feature => {
+        return ((typeof feature.style_?.getImage() !== "undefined" && feature.style_?.getImage() !== null) || (typeof feature.style_?.getStroke() !== "undefined" && feature.style_?.getStroke() !== null) || (typeof feature.style_?.getFill() !== "undefined" && feature.style_?.getFill() !== null)) && !isFeatureDisabled(feature);
+    }
 };
