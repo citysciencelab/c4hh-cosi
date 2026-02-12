ed4bd4999b13178af6e2b58424484decee70a749
Duden <DudenSe-Admin@fhhnet.stadt.hamburg.de>
2026-02-12 10:44:58 +0100

    G31DEV1-3709-Dashboard-Update Charts only display available year data.

--- cosi/Dashboard/components/DashboardChartView.vue
+++ cosi/Dashboard/components/DashboardChartView.vue
@@ -11,7 +11,7 @@ export default {
         ChartItem
     },
     computed: {
-        ...mapGetters("Modules/Dashboard", ["districtColumns", "items", "statsFeatureFilter", "timestampPrefix", "timestamps", "timestampsFiltered"]),
+        ...mapGetters("Modules/Dashboard", ["districtColumns", "items", "statsFeatureFilter", "timestampPrefix", "timestamps"]),
 
         /**
          * Gets the items grouped by their group property.
@@ -42,14 +42,6 @@ export default {
                 groups[item.group].push(item);
             });
             return groups;
-        },
-
-        /**
-         * Gets the timestamps to use in the charts. All timestamps if none are filtered.
-         * @returns {Number[]} The timestamps to use
-         */
-        timestampsToUse () {
-            return this.timestampsFiltered.length > 0 ? this.timestampsFiltered : this.timestamps.toSorted();
         }
     },
     methods: {
@@ -57,18 +49,19 @@ export default {
 
         /**
          * Gets the barchart data for a given group.
-         * @param {Object} group The group to get data for
-         * @param {Object[]} districtColumns The district columns to use
-         * @param {Object[]} timestampsToUse The timestamps to use
-         * @param {String} timestampPrefix The prefix for the timestamps
+         * @param {Object} group - The group to get data for
+         * @param {Object[]} districtColumns - The district columns to use
+         * @param {String} timestamp - The timestamp to use for the barchart data
+         * @param {String} timestampPrefix - The prefix for the timestamps
+         * @returns {Object[]} The barchart data for the group
          */
-        getBarchartDataForGroup (group, districtColumns, timestampsToUse, timestampPrefix) {
+        getBarchartDataForGroup (group, districtColumns, timestamp, timestampPrefix) {
             return group.map(item => ({
                 name: item.category,
                 data: [
                     Object.fromEntries(districtColumns.map(column => [
                         column.value,
-                        item[column.value]?.[`${timestampPrefix}${timestampsToUse[0]}`]
+                        item[column.value]?.[`${timestampPrefix}${timestamp}`]
                     ]))
                 ]
             }));
@@ -76,21 +69,73 @@ export default {
 
         /**
          * Gets the linechart data for a given group.
-         * @param {Object} group The group to get data for
-         * @param {Object[]} districtColumns The district columns to use
-         * @param {Object[]} timestampsToUse The timestamps to use
-         * @param {String} timestampPrefix The prefix for the timestamps
+         * @param {Object} group - The group to get data for
+         * @param {Object[]} districtColumns - The district columns to use
+         * @param {String} timestampPrefix - The prefix for the timestamps
+         * @returns {Object[]} The linechart data for the group
          */
-        getLinechartDataForGroup (group, districtColumns, timestampsToUse, timestampPrefix) {
+        getLinechartDataForGroup (group, districtColumns, timestampPrefix) {
             return group.map(item => ({
                 name: item.category,
                 data: districtColumns.map(
-                    column => Object.fromEntries(timestampsToUse.map(timestamp => [
-                        timestamp,
-                        item[column.value]?.[`${timestampPrefix}${timestamp}`]
-                    ]))
+                    column => Object.fromEntries(item.years
+                        .filter(timestamp => typeof item[column.value]?.[`${timestampPrefix}${timestamp}`] !== "undefined") // Filtere nur gültige Werte
+                        .map(timestamp => [
+                            timestamp,
+                            item[column.value]?.[`${timestampPrefix}${timestamp}`]
+                        ]))
                 )
             }));
+        },
+
+        /**
+         * Returns the chart data for the specified group and district columns based on the chart mode.
+         * Determines whether to generate data for a bar chart or a line chart.         *
+         * @param {Object[]} group - The group object containing data for the chart.
+         * @param {Object[]} districtColumns - An array of district column identifiers.
+         * @returns {Object[]} The chart data formatted for the specified chart mode (bar or line).
+         */
+        getChartData (group, districtColumns) {
+            const chartMode = this.getChartMode(group, districtColumns);
+
+            if (chartMode.type === "bar") {
+                return this.getBarchartDataForGroup(group, districtColumns, chartMode.timestamps[0], this.timestampPrefix);
+            }
+            return this.getLinechartDataForGroup(group, districtColumns, this.timestampPrefix);
+        },
+
+        /**
+         * Determines the chart mode (either "bar" or "line") based on the uniqueness of timestamps.
+         * @param {Object[]} group - The grouped array of dashboard item objects.
+         * @param {Object[]} districtColumns - The array of dashboard column objects.
+         * @returns {Object} An object containing the unique timestamps and the determined chart type ("bar" or "line").
+         */
+        getChartMode (group, districtColumns) {
+            const uniqueTimestamps = this.getUniqueTimestamps(group, districtColumns, this.timestampPrefix);
+
+            return {
+                timestamps: uniqueTimestamps,
+                type: uniqueTimestamps.length === 1 ? "bar" : "line"
+            };
+        },
+
+        /**
+         * Extracts unique timestamps from a group of data based on specified district columns and a timestamp prefix.         *
+         * @param {Object[]} group - The grouped array of dashboard item objects.
+         * @param {Object[]} districtColumns - The array of dashboard column objects.
+         * @param {String} timestampPrefix - The prefix to prepend to each timestamp for lookup.
+         * @returns {String[]} - An array of unique timestamps.
+         */
+        getUniqueTimestamps (group, districtColumns, timestampPrefix) {
+            const timestamps = group.flatMap(
+                item => districtColumns.flatMap(
+                    column => item.years.filter(
+                        timestamp => typeof item[column.value]?.[`${timestampPrefix}${timestamp}`] !== "undefined"
+                    )
+                )
+            );
+
+            return [...new Set(timestamps)];
         }
     }
 };
@@ -106,13 +151,9 @@ export default {
     >
         <ChartItem
             class="mt-4 mb-5"
-            :chart-mode="timestampsToUse.length === 1
-                ? 'bar'
-                : 'line'"
-            :data="timestampsToUse.length === 1
-                ? getBarchartDataForGroup(group, districtColumns, timestampsToUse, timestampPrefix)
-                : getLinechartDataForGroup(group, districtColumns, timestampsToUse, timestampPrefix)"
-            :show-x-values-filter="timestampsToUse.length > 1"
+            :chart-mode="getChartMode(group, districtColumns, timestampPrefix).type"
+            :data="getChartData(group, districtColumns)"
+            :show-x-values-filter="getChartMode(group, districtColumns).type === 'line'"
             :x-values-filter-label="$t('additional:modules.tools.cosi.dashboard.year')"
         />
     </AccordionItem>
