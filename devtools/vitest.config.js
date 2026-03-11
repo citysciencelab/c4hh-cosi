d7d25cfbd6ca33d4b76d959950891136d114bde3
Mohammad Reaza Mossadegh <mohammadreza.mossadegh@gv.hamburg.de>
2026-03-11 13:49:16 +0100

    G31DEV1-3823 - fix tests

--- devtools/vitest.config.js
+++ devtools/vitest.config.js
@@ -54,7 +54,6 @@ export default defineConfig(mode => mergeConfig(
             ],
             exclude: [
                 // an G31: https://lgv-hamburg.atlassian.net/browse/G31DEV1-3566
-                "**/VerkehrsstaerkenTheme.spec.js",
                 "**/SimulationParameter.spec.js",
                 "**/FilterGeneral.spec.js",
                 // defaults from vitest:
