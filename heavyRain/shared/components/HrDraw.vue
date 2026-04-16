4492d3e3ee672d83910f82a6d8275970ac040d22
Feng Feng <feng.feng@gv.hamburg.de>
2026-04-16 17:15:49 +0200

    G31DEV1-3883 Add: icon button for editting the geometry

--- heavyRain/shared/components/HrDraw.vue
+++ heavyRain/shared/components/HrDraw.vue
@@ -4,7 +4,8 @@ import GeoJSON from "ol/format/GeoJSON.js";
 import IconButton from "@shared/modules/buttons/components/IconButton.vue";
 import layerCollection from "@core/layers/js/layerCollection.js";
 import layerFactory from "@core/layers/js/layerFactory.js";
-import {markRaw} from "vue";
+import {mapActions} from "vuex";
+import modifyInteraction from "@masterportal/masterportalapi/src/maps/interactions/modifyInteraction.js";
 
 const hrDrawLayerId = "heavy-rain-draw";
 
@@ -29,6 +30,7 @@ export default {
                 strokeColor: [255, 2, 2],
                 strokeWidth: 2
             },
+            currentModifyInteraction: null,
             drawTypeLabels: [
                 {type: "polygon", label: "common:shared.modules.draw.drawTypes.polygon"},
                 {type: "box", label: "common:shared.modules.draw.drawTypes.box"},
@@ -36,20 +38,72 @@ export default {
             ],
             source: null,
             selectedDrawType: "",
-            selectedDrawTypeMain: ""
+            selectedDrawTypeMain: "",
+            selectedInteraction: null
         };
     },
+    computed: {
+        /**
+         * Gets if to show the edit geometry button.
+         * @returns {Boolean} True if there is drawn feature.
+         */
+        showEditIcon () {
+            return typeof this.source?.getFeatures === "function" && this.source?.getFeatures()?.length;
+        }
+    },
+    watch: {
+        /**
+         * Decides if to remove edit interaction according to selected draw type.
+         * @param {String} val the main selected draw type.
+         */
+        selectedDrawTypeMain (val) {
+            if (val !== "") {
+                this.removeInteraction(this.currentModifyInteraction);
+                this.currentModifyInteraction = null;
+            }
+        }
+    },
     created () {
-        this.source = markRaw(this.getLayerSource());
+        this.source = this.getLayerSource();
+    },
+    unmounted () {
+        this.removeInteraction(this.currentModifyInteraction);
+        this.currentModifyInteraction = null;
     },
     methods: {
+        ...mapActions("Maps", ["addInteraction", "removeInteraction"]),
+
         /**
          * Clears the current drawing and emits an empty value.
          * @returns {void}
          */
         clearDrawnFeature () {
-            this.source?.clear();
+            this.resetAll();
             this.$emit("update:drawn-geojson-feature", null);
+        },
+
+        /**
+         * Edits the geometry of current source features.
+         * @returns {void}
+         */
+        editSource () {
+            if (this.currentModifyInteraction === null) {
+                this.removeInteraction(this.selectedInteraction);
+                this.selectedDrawType = "";
+                this.selectedDrawTypeMain = "";
+                this.selectedInteraction = "";
+                this.currentModifyInteraction = modifyInteraction.createModifyInteraction(this.source);
+                this.addInteraction(this.currentModifyInteraction);
+                this.currentModifyInteraction?.on("modifyend", async () => {
+                    const geojson = new GeoJSON().writeFeatureObject(this.source.getFeatures()[0]);
+
+                    this.$emit("update:drawn-geojson-feature", geojson);
+                });
+            }
+            else {
+                this.removeInteraction(this.currentModifyInteraction);
+                this.currentModifyInteraction = null;
+            }
         },
 
         /**
@@ -61,8 +115,14 @@ export default {
             if (!event?.feature) {
                 return;
             }
+
+            if (typeof event.feature.setId === "function") {
+                event.feature.setId("drawn-feature");
+            }
+
             const geojsonFeature = new GeoJSON().writeFeatureObject(event.feature);
 
+            this.source.addFeature(event.feature);
             this.$emit("update:drawn-geojson-feature", geojsonFeature);
         },
 
@@ -87,6 +147,16 @@ export default {
             layerCollection.addLayer(layer);
 
             return layer.getLayerSource();
+        },
+
+        /**
+         * Clears the current draw interaction and deletes the source.
+         * @returns {void}
+         */
+        resetAll () {
+            this.source.clear();
+            this.removeInteraction(this.currentModifyInteraction);
+            this.currentModifyInteraction = null;
         }
     }
 };
@@ -107,10 +177,11 @@ export default {
                 :draw-type-labels="drawTypeLabels"
                 :selected-draw-type="selectedDrawType"
                 :selected-draw-type-main="selectedDrawTypeMain"
+                :selected-interaction="selectedInteraction"
                 :source
                 :set-selected-draw-type="value => selectedDrawType = value"
                 :set-selected-draw-type-main="value => selectedDrawTypeMain = value"
-                @drawstart="() => source?.clear()"
+                @drawstart="resetAll"
                 @drawend="onDrawEnd"
             />
             <IconButton
@@ -119,6 +190,17 @@ export default {
                 icon="bi bi-trash"
                 :interaction="clearDrawnFeature"
                 :label="$t('common:modules.draw_old.attributeSelect.remove')"
+            />
+            <IconButton
+                v-if="showEditIcon"
+                :class-array="[
+                    'btn-primary',
+                    currentModifyInteraction !== null ? 'active': '',
+                ]"
+                :aria="$t('additional:modules.updateRequirements.geometryEdit')"
+                icon="bi bi-tools"
+                :interaction="editSource"
+                :label="$t('additional:modules.updateRequirements.geometryEdit')"
             />
         </div>
     </div>
--- heavyRain/shared/tests/components/HrDraw.spec.js
+++ heavyRain/shared/tests/components/HrDraw.spec.js
@@ -1,25 +1,44 @@
 import {config, shallowMount} from "@vue/test-utils";
+import {createStore} from "vuex";
 import {expect} from "chai";
 import sinon from "sinon";
 import GeoJSON from "ol/format/GeoJSON.js";
 import HrDraw from "../../components/HrDraw.vue";
 import layerCollection from "@core/layers/js/layerCollection.js";
 import layerFactory from "@core/layers/js/layerFactory.js";
+import modifyInteraction from "@masterportal/masterportalapi/src/maps/interactions/modifyInteraction.js";
 
 config.global.mocks.$t = key => key;
 
 describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
-    let source,
+    let store,
+        source,
         layer,
         geojsonFeature,
         getLayerByIdStub,
         addLayerStub,
+        addInteractionStub,
         createLayerStub,
+        createModifyInteractionStub,
+        removeInteractionStub,
         writeFeatureObjectStub;
 
     beforeEach(() => {
+        store = createStore({
+            modules: {
+                Maps: {
+                    namespaced: true,
+                    actions: {
+                        addInteraction: sinon.stub(),
+                        removeInteraction: sinon.stub()
+                    }
+                }
+            }
+        });
         source = {
-            clear: sinon.stub()
+            clear: sinon.stub(),
+            addFeature: sinon.stub(),
+            getFeatures: sinon.stub()
         };
         layer = {
             getLayerSource: sinon.stub().returns(source)
@@ -34,8 +53,11 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
 
         getLayerByIdStub = sinon.stub(layerCollection, "getLayerById").returns(layer);
         addLayerStub = sinon.stub(layerCollection, "addLayer");
+        addInteractionStub = sinon.stub(HrDraw.methods, "addInteraction");
         createLayerStub = sinon.stub(layerFactory, "createLayer").returns(layer);
         writeFeatureObjectStub = sinon.stub(GeoJSON.prototype, "writeFeatureObject").returns(geojsonFeature);
+        removeInteractionStub = sinon.stub(HrDraw.methods, "removeInteraction");
+        createModifyInteractionStub = sinon.stub(modifyInteraction, "createModifyInteraction");
     });
 
     afterEach(() => {
@@ -44,15 +66,34 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
 
     describe("Component DOM", () => {
         it("should exist", () => {
-            const wrapper = shallowMount(HrDraw);
+            const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
 
             expect(wrapper.exists()).to.be.true;
         });
 
         it("should render DrawTypes in the template", () => {
-            const wrapper = shallowMount(HrDraw);
+            const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
 
             expect(wrapper.findComponent({name: "DrawTypes"}).exists()).to.be.true;
+        });
+
+        it("should render a delete IconButton in the template", () => {
+            const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
+
+            expect(wrapper.findAllComponents({name: "IconButton"})).to.be.lengthOf(1);
+            expect(wrapper.findAllComponents({name: "IconButton"}).at(0).vm.label).to.equal("common:modules.draw_old.attributeSelect.remove");
+        });
+
+        it("should render an edit IconButton in the template", async () => {
+            const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
+
+            await wrapper.setData({source: {
+                clear: sinon.stub(),
+                getFeatures: () => ["1"]
+            }});
+
+            expect(wrapper.findAllComponents({name: "IconButton"})).to.be.lengthOf(2);
+            expect(wrapper.findAllComponents({name: "IconButton"}).at(1).vm.label).to.equal("additional:modules.updateRequirements.geometryEdit");
         });
     });
 
@@ -60,7 +101,7 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
         it("should create a new layer and assign its source in created", () => {
             getLayerByIdStub.returns(undefined);
 
-            const wrapper = shallowMount(HrDraw);
+            const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
 
             expect(getLayerByIdStub.calledOnceWith("heavy-rain-draw")).to.be.true;
             expect(createLayerStub.calledOnce).to.be.true;
@@ -71,14 +112,14 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
                 alwaysOnTop: true
             });
             expect(addLayerStub.calledOnceWith(layer)).to.be.true;
-            expect(wrapper.vm.source).to.equal(source);
+            expect(wrapper.vm.source).to.deep.equal(source);
         });
     });
 
     describe("methods", () => {
         describe("clearDrawnFeature", () => {
             it("should clear the source and emit null", () => {
-                const wrapper = shallowMount(HrDraw);
+                const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
 
                 wrapper.vm.clearDrawnFeature();
 
@@ -87,12 +128,38 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
             });
         });
 
+        describe("editSource", () => {
+            it("should trigger some interaction function if the current interaction is null", () => {
+                const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
+
+                wrapper.vm.editSource();
+
+                expect(wrapper.vm.selectedDrawType).to.equal("");
+                expect(wrapper.vm.selectedDrawTypeMain).to.equal("");
+                expect(wrapper.vm.selectedInteraction).to.equal("");
+                expect(addInteractionStub.calledOnce).to.be.true;
+                expect(createModifyInteractionStub.calledOnce).to.be.true;
+                expect(removeInteractionStub.calledOnce).to.be.true;
+            });
+
+            it("should trigger removeInteraction function and set current intraction into null if the current interaction is not null", async () => {
+                const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
+
+                await wrapper.setData({currentModifyInteraction: {}});
+
+                wrapper.vm.editSource();
+
+                expect(wrapper.vm.currentModifyInteraction).to.equal(null);
+                expect(removeInteractionStub.calledOnce).to.be.true;
+            });
+        });
+
         describe("onDrawEnd", () => {
             it("should emit a GeoJSON object", () => {
                 const feature = {id: "feature-1"},
-                    wrapper = shallowMount(HrDraw);
+                    wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
 
-                wrapper.vm.onDrawEnd({feature});
+                wrapper.vm.onDrawEnd({"feature": feature});
 
                 expect(writeFeatureObjectStub.calledOnceWith(feature)).to.be.true;
                 expect(wrapper.emitted("update:drawn-geojson-feature")).to.deep.equal([[geojsonFeature]]);
@@ -119,6 +186,18 @@ describe("addons/heavyRain/shared/components/HrDraw.vue", () => {
                 expect(createLayerStub.notCalled).to.be.true;
                 expect(addLayerStub.notCalled).to.be.true;
                 expect(result).to.equal(source);
+            });
+        });
+
+        describe("resetAll", () => {
+            it("should reset all the data and source", () => {
+                const wrapper = shallowMount(HrDraw, {global: {plugins: [store]}});
+
+                wrapper.vm.resetAll();
+
+                expect(source.clear.calledOnce).to.be.true;
+                expect(wrapper.vm.currentModifyInteraction).to.equal(null);
+                expect(removeInteractionStub.calledOnce).to.be.true;
             });
         });
     });
