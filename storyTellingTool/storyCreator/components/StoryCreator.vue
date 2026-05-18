5289ca611183c7d6c5752c9f818e6140f0287e28
Mohammad Reaza Mossadegh <mohammadreza.mossadegh@gv.hamburg.de>
2026-05-18 14:53:28 +0200

    G31DEV1-3987 - add infos story

--- storyTellingTool/storyCreator/components/StoryCreator.vue
+++ storyTellingTool/storyCreator/components/StoryCreator.vue
@@ -1,21 +1,72 @@
 <script>
+import FileUpload from "@shared/modules/inputs/components/FileUpload.vue";
+import InputText from "@shared/modules/inputs/components/InputText.vue";
 import StoryCreatorChapter from "./StoryCreatorChapter.vue";
 
 export default {
     name: "StoryCreator",
     components: {
+        FileUpload,
+        InputText,
         StoryCreatorChapter
+    },
+    data () {
+        return {
+            storyName: "Geschichten mit Karten erzählen",
+            storyDescription: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
+            altText: "Blick über die Hamburger Elbphilharmonie",
+            imageSource: "Max Mustermann / Getty Images",
+            selectedFile: null
+        };
     }
 };
 </script>
 
 <template lang="html">
-    <div>
+    <div id="story-creator">
+        <p class="mb-4">
+            {{ $t("additional:modules.storyCreator.introText") }}
+        </p>
+        <InputText
+            id="storyName"
+            v-model="storyName"
+            :label="$t('additional:modules.storyCreator.labels.storyName')"
+            :placeholder="$t('additional:modules.storyCreator.labels.storyName')"
+            class="mb-3"
+        />
+        <InputText
+            id="storyDescription"
+            v-model="storyDescription"
+            :label="$t('additional:modules.storyCreator.labels.storyDescription')"
+            :placeholder="$t('additional:modules.storyCreator.labels.storyDescription')"
+            html-type="textarea"
+            class="mb-3"
+        />
+        <FileUpload
+            id="Story-creator-file-upload"
+            :label="$t('additional:modules.storyCreator.labels.fileDrop')"
+            :sub-label="$t('additional:modules.storyCreator.labels.fileSelect')"
+            class="mb-3"
+            :change="() => undefined"
+            :drop="() => undefined"
+        />
+        <InputText
+            id="altText"
+            v-model="altText"
+            :label="$t('additional:modules.storyCreator.labels.altText')"
+            :placeholder="$t('additional:modules.storyCreator.labels.altText')"
+            class="mb-3"
+        />
+        <InputText
+            id="imageSource"
+            v-model="imageSource"
+            :label="$t('additional:modules.storyCreator.labels.imageSource')"
+            :placeholder="$t('additional:modules.storyCreator.labels.imageSource')"
+            class="mb-3"
+        />
         <StoryCreatorChapter />
     </div>
 </template>
-
-
 <style lang="scss" scoped>
 </style>
 
--- storyTellingTool/storyCreator/tests/unit/components/StoryCreator.spec.js
+++ storyTellingTool/storyCreator/tests/unit/components/StoryCreator.spec.js
@@ -6,10 +6,26 @@ config.global.mocks.$t = key => key;
 
 describe("addons/storyCreator/components/storyCreator.vue", () => {
     describe("Component DOM", () => {
-        it("should exist", () => {
-            const wrapper = shallowMount(StoryCreator);
+        const wrapper = shallowMount(StoryCreator);
 
+        it("should exist", () => {
             expect(wrapper.exists()).to.be.true;
+        });
+        it("should render exactly one FileUpload component", () => {
+            const fileUpload = wrapper.findComponent({name: "FileUpload"});
+
+            expect(fileUpload.exists()).to.be.true;
+            expect(fileUpload.attributes("id")).to.equal("Story-creator-file-upload");
+        });
+        it("should render all required InputText fields", () => {
+            const allInputTexts = wrapper.findAllComponents({name: "InputText"});
+
+            expect(allInputTexts.length).to.equal(4);
+        });
+        it("should render the InputText component used for the story description", () => {
+            const descriptionInput = wrapper.findAllComponents({name: "InputText"});
+
+            expect(descriptionInput.at(1).attributes("id")).to.equal("storyDescription");
         });
     });
 });
