4fa0b9d4a6c49fc9329bc350822e04c4820f0c32
Inka Dudek <inka.dudek@gv.hamburg.de>
2026-05-22 09:56:40 +0200

    correction afetr rebase

--- boris/tests/unit/store/gettersBoris.spec.js
+++ boris/tests/unit/store/gettersBoris.spec.js
@@ -17,9 +17,6 @@ describe("src/modules/tools/boris/store/gettersBoris.js", () => {
         stateBoris.selectedPolygon = stateOrig.selectedPolygon;
         stateBoris.paramUrlParams = stateOrig.paramUrlParams;
     });
-    beforeEach(() => {
-        sinon.stub(console, "warn").callsFake(sinon.spy());
-    });
 
     describe("Boris getters", () => {
         it("findLanduseByBrwId", () => {
