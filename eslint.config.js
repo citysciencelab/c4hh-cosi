017418067b798ac63c5e3c1eb115ad762d0338be
Manuela Maiwald <manuela.maiwald@gv.hamburg.de>
2026-06-18 11:21:00 +0200

    trailing comma fix

--- eslint.config.js
+++ eslint.config.js
@@ -385,7 +385,7 @@ export default [
                 {
                     selector: "VElement[name='li']:not([parent.name='ol']):not([parent.name='ul']):not([parent.name='menu']):not([parent.name='template']):not([parent.name='transitiongroup'])",
                     message: "<li> must be a direct child of <ol>, <ul>, <menu>, or <template>. Using other parents violates accessibility standards. Vue's <TransitionGroup> is allowed when it renders as a list container (<ul> or <ol>)."
-                },
+                }
             ]
 
 
